e4d9c24056b159d2cc212e0e2ba737f6796a39d7
Katie <paikk662@newschool.edu>
2018-10-30 11:20:36 -0400

    intro to js

---
+++ labs/lab6a/assets/js/app.js
@@ -0,0 +1,13 @@
+var name = "Katie";
+var age = 19;
+var can_drive = true;
+
+var dresser = [['socks', 25], 'shirts', 'pants', 'old stuff'];
+
+//function
+
+function hello_world() {
+    alert('Hello World');
+}
+
+hello_world();
